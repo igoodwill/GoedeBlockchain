8ff322d37868d139dfb88783f2653b0a2d8761c2
igoodwill <igor.goodwill1@gmail.com>
2018-05-25 22:04:31 +0300

    Send Data
    "Send Data" page has been added.

--- js/pages/send-data.js
+++ js/pages/send-data.js
@@ -1,10 +1,46 @@
 const m = require('mithril')
 
 module.exports = {
-	view: function () {
-    	return m("div", [
+	data: "",
+    receiverAddress: "",
+    setData: function (val) {
+        this.data = val
+    },
+    setReceiverAddress: function (val) {
+        this.receiverAddress = val
+    },
+    sendData: function () {
+        // Call here a method that sends the data.
+    },
+    view: function () {
+        return m("div", [
     		m("h4", {class: "title"}, "Send Data"),
-    		m("br"),
+    		m("div", {class: "row"}, [
+                m("input", {
+                    class: "six columns",
+                    type: "text",
+                    placeholder: "Data",
+                    name: "data",
+                    oninput: m.withAttr("value", this.setData.bind(this)),
+                    value: this.data
+                })
+            ]),
+            m("div", {class: "row"}, [
+                m("input", {
+                    class: "six columns",
+                    type: "email",
+                    placeholder: "Send to",
+                    name: "receiverAddress",
+                    oninput: m.withAttr("value", this.setReceiverAddress.bind(this)),
+                    value: this.receiverAddress
+                })
+            ]),
+            m("div", {class: "row"}, [
+                m("button", {
+                	class: "button-primary",
+                	onclick: this.sendData.bind(this)},
+                	"Send")
+            ]),
             m("div", {class: "row"}, [
                 m("a", {href: "#!/wallet"}, "Back to wallet")
             ])
