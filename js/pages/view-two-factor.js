53ff940b87889a465d79449561764bf620968fc5
igoodwill <igor.goodwill1@gmail.com>
2018-06-01 19:58:09 +0300

    2FA page update

--- js/pages/view-two-factor.js
+++ js/pages/view-two-factor.js
@@ -3,12 +3,20 @@ const m = require('mithril')
 module.exports = {
     view: function () {
         return m("div", [
-            m("h4", {class: "title"}, "Two-Factor authentication data"),
+            m("h4", {class: "title"}, "Two-Factor authentication"),
             m("hr"),
+            m("pre",
+                "1. Download and open \"Google Authenticator\" on your mobile device, and log in.\n" +
+                "2. Press \"+\" icon at the bottom right corner.\n" +
+                "3. Choose the \"Scan a barcode\" option and scan the barcode:"
+            ),
             m("div", {class: "row"}, [
                 m("img", {src: global.filesystem.otp.qr})
             ]),
-            m("p", global.filesystem.otp.secret.ascii),
+            m("pre",
+                "... or choose the \"Enter provided key\" option and input the key: " + global.filesystem.otp.secret.ascii +
+                "\n4. Use the code from the application to log into your Goede Blockchain account."
+            ),
             m("div", {class: "back"}, [
                 m("a", {href: "#!/wallet"}, "Back to wallet")
             ])
